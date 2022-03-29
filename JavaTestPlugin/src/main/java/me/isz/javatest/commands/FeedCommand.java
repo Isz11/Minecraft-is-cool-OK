8a86dfc48515548a9152d6cc5d8f7e70d91926d2
Ben <ben-mcleod@live.co.uk>
2022-03-29 17:57:54 +0100

    Command and customisable config message testing

---
+++ JavaTestPlugin/src/main/java/me/isz/javatest/commands/FeedCommand.java
@@ -0,0 +1,35 @@
+package me.isz.javatest.commands;
+
+import me.isz.javatest.JavaTest;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class FeedCommand implements CommandExecutor {
+
+    Plugin plugin = JavaTest.getPlugin(JavaTest.class); // this extends the main class to allow grabbing of config values
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        String feed_allow = plugin.getConfig().getString("feed");
+        String feed_deny = plugin.getConfig().getString("feed");
+
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+
+            if(p.hasPermission("javatestplugin.feed")) {
+                p.setSaturation(50);
+                p.setFoodLevel(20);
+
+                p.sendMessage(feed_allow);
+            } else {
+                p.sendMessage(feed_deny);
+            }
+        }
+        return true;
+    }
+}
