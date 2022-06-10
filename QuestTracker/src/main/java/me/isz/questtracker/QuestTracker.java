88afe1ca03013b36e612dac02d9918482cf6d254
Ben Mcleod <ben-mcleod@live.co.uk>
2022-06-10 21:05:33 +0100

    Initial commit

---
+++ QuestTracker/src/main/java/me/isz/questtracker/QuestTracker.java
@@ -0,0 +1,17 @@
+package me.isz.questtracker;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class QuestTracker extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
